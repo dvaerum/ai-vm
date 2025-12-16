e19d939347354f4b09788f766a0dda57433e7f4d
Dennis Vestergaard Værum <github@varum.dk>
2025-12-16 20:26:45 +0100

    Enabled nix-ld to add support for building android apps

--- nixos/modules/configuration.nix
+++ nixos/modules/configuration.nix
@@ -59,4 +59,25 @@
       Persistent = true;
     };
   };
+
+  # Enabled so that one can build android apps
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      zlib
+      zstd
+      stdenv.cc.cc
+      curl
+      openssl
+      attr
+      libssh
+      bzip2
+      libxml2
+      acl
+      libsodium
+      util-linux
+      xz
+      systemd
+    ];
+  };
 }
